33654ecd1b61d848bf49c02b46f43daf84ac2260
Iyron Woodhouse <iyronwoodhouse@gmail.com>
2020-02-27 10:12:34 -0800

    finished Services

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,9 +3,11 @@ import Hero from '../components/Hero'
 import Banner from '../components/Banner'
 import { Link } from 'react-router-dom'
 
+import Services from '../components/Services'
+
 export default function Home() {
   return (
-    <div>
+    <>
       <Hero hero="defaultHero">
         <Banner title="luxurious rooms" subtitle="deluxe rooms starting at $299">
           <Link to="/rooms" className="btn-primary">
@@ -13,6 +15,7 @@ export default function Home() {
           </Link>
         </Banner>
       </Hero>
-    </div>
+      <Services />
+    </>
   )
 }

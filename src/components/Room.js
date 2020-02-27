14d065cfa12b94fd790f88996a3cebe959f54789
Iyron Woodhouse <iyronwoodhouse@gmail.com>
2020-02-27 14:08:27 -0800

    Finished Room.js

--- src/components/Room.js
+++ src/components/Room.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
-export default function Room() {
+export default function Room({room}) {
+  const { name, images, price, slug } = room;
+
   return (
-    <div>
-      Hello from Room
-    </div>
+    <article className="room">
+      <div className="img-container">
+        <img src={images[0]} alt="room-img"/>
+        <div className="price-top">
+          <h6>${price}</h6>
+          <p>per night</p>
+        </div>
+        <Link to={`/rooms/${slug}`} className="btn-primary room-link">
+          features
+        </Link>
+      </div>
+      <p className="room-info">{name}</p>
+    </article>
   )
+}
+
+Room.propTypes = {
+  room: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    images: PropTypes.arrayOf(PropTypes.string).isRequired,
+    price: PropTypes.number.isRequired,
+    slug: PropTypes.string.isRequired,
+  })
 }

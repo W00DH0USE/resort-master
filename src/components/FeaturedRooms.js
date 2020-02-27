a5e0f6bc8e12ed7bdc3e66b850d4c364e525337b
Iyron Woodhouse <iyronwoodhouse@gmail.com>
2020-02-27 10:53:40 -0800

    Set up context.js

---
+++ src/components/FeaturedRooms.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import { RoomContext } from '../context'
+
+export default class FeaturedRooms extends Component {
+  static contextType = RoomContext
+
+  render() {
+    const value = this.context
+
+    return (
+      <div>
+        Hello from Featured Rooms
+        <p></p>
+      </div>
+    )
+  }
+}

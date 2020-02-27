10aef88f95c0926888ddc763ffd83f8ef25780e8
Iyron Woodhouse <iyronwoodhouse@gmail.com>
2020-02-27 11:22:57 -0800

    Finished Loading.js

--- src/components/FeaturedRooms.js
+++ src/components/FeaturedRooms.js
@@ -1,16 +1,21 @@
 import React, { Component } from 'react'
 import { RoomContext } from '../context'
 
+import Loading from './Loading';
+
 export default class FeaturedRooms extends Component {
   static contextType = RoomContext
 
   render() {
-    const value = this.context
+    const { featuredRooms: rooms } = this.context
+    console.log(rooms);
+    
 
     return (
       <div>
         Hello from Featured Rooms
         <p></p>
+        <Loading />
       </div>
     )
   }
